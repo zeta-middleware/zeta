debbebc0c85fbde5dddffa6c30db4bccd43adbb2
Lucas Peixoto <lucaspeixotoac@gmail.com>
2020-03-20 21:12:36 -0300

    Implement the basic of pdb.c
    Signed-off-by: Lucas Peixoto <lucaspeixotoac@gmail.com>

--- src/pdb.c
+++ src/pdb.c
@@ -52,3 +52,132 @@ pdb_property_t *pdb_property_get_ref(pdb_property_e property)
         return NULL;
     }
 }
+
+int pdb_property_get(pdb_property_e id, u8_t *property_value, size_t size)
+{
+    int error               = 0;
+    pdb_property_t *property = pdb_property_get_ref(id);
+    if (property && property->get) {
+        error = property->get(id, property_value, size);
+        if (error) {
+            printk("Current property get: %d, error code: %d\n", id, error);
+        }
+    } else {
+        printk("The property #%d does not have a get implementation.\n", id);
+    }
+    return error;
+}
+
+int pdb_property_get_private(pdb_property_e id, u8_t *property_value, size_t size)
+{
+    return 0;
+}
+
+int pdb_property_set(pdb_property_e id, u8_t *property_value, size_t size)
+{
+    int error               = 0;
+    int valid               = 1;
+    pdb_property_t *property = pdb_property_get_ref(id);
+    if (property && property->set) {
+        if (property->validate) {
+            valid = property->validate(property_value, size);
+        }
+        if (valid) {
+            error = property->set(id, property_value, size);
+            if (error) {
+                printk("Current property set: %d, error code: %d\n", id, error);
+            }
+        } else {
+            error = -EINVAL;
+        }
+    } else {
+        printk("The property %d is read only.\n", id);
+        error = -ENODEV;
+    }
+    return error;
+}
+
+int pdb_property_set_private(pdb_property_e id, u8_t *property_value, size_t size)
+{
+    return 0;
+}
+
+
+u8_t pdb_u8(pdb_property_e id, int *error)
+{
+    u8_t temp = 0;
+    if (error) {
+        *error = pdb_property_get(id, PDB_REF(temp));
+    } else {
+        pdb_property_get(id, PDB_REF(temp));
+    }
+    return temp;
+}
+u16_t pdb_u16(pdb_property_e id, int *error)
+{
+    u16_t temp = 0;
+    if (error) {
+        *error = pdb_property_get(id, PDB_REF(temp));
+    } else {
+        pdb_property_get(id, PDB_REF(temp));
+    }
+    return temp;
+}
+u32_t pdb_u32(pdb_property_e id, int *error)
+{
+    u32_t temp = 0;
+    if (error) {
+        *error = pdb_property_get(id, PDB_REF(temp));
+    } else {
+        pdb_property_get(id, PDB_REF(temp));
+    }
+    return temp;
+}
+u64_t pdb_u64(pdb_property_e id, int *error)
+{
+    u64_t temp = 0;
+    if (error) {
+        *error = pdb_property_get(id, PDB_REF(temp));
+    } else {
+        pdb_property_get(id, PDB_REF(temp));
+    }
+    return temp;
+}
+
+int pdb_buffer_clear(pdb_buffer_t *buffer)
+{
+    memset(buffer->content, 0, PDB_MAX_PROPERTY_SIZE);
+    buffer->size = 0;
+    return 0;
+}
+
+int pdb_buffer_push_data(pdb_buffer_t *buffer, u8_t *data, size_t size)
+{
+    if (size > 0 && ((buffer->size + size) <= PDB_MAX_PROPERTY_SIZE)) {
+        u8_t *ptr = &buffer->content[0] + buffer->size;
+        memcpy(ptr, data, size);
+        buffer->size += (u8_t) size;
+        return 0;
+    }
+    return -EINVAL;
+}
+
+int pdb_buffer_pop_data(pdb_buffer_t *buffer, u8_t *data, size_t size)
+{
+    u8_t *ptr  = &buffer->content[0];
+    s32_t jump = buffer->size - size;
+    if (jump >= 0) {
+        ptr = ptr + jump;
+        memcpy((void *) data, (void *) ptr, size);
+        memset((void *) ptr, 0, size);
+        buffer->size -= size;
+        return 0;
+    }
+    return -EINVAL;
+}
+
+void pdb_thread()
+{
+    return;
+}
+

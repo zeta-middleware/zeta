3fd6d9cf44209b27dae1817e4f656cc74be6b8be
Lucas Peixoto <lucaspeixotoac@gmail.com>
2020-03-24 15:16:26 -0300

    Change files generated to inside of build folder and finish script
    Signed-off-by: Lucas Peixoto <lucaspeixotoac@gmail.com>

--- generate_properties.py
+++ generate_properties.py
@@ -5,10 +5,11 @@ from os import getcwd
 
 class PropertyCreateGeneration :
     def __init__(self) :
-        self.properties_macro_create = ""
-        self.validators_h_create = ""
-        self.callbacks_h_create = ""
-        self.observers_h_create = ""
+        self.properties_create = ""
+        self.validators_def = ""
+        self.callbacks = ""
+        self.observers_def = ""
+        self.observers_check = ""
 
     def generate_queues(self, obs) :
         ret = list()
@@ -21,7 +22,7 @@ class PropertyCreateGeneration :
         # return ' | '.join(l)
 
     def generate_property_create(self, p) :
-        self.properties_macro_create += "PDB_PROPERTY_CREATE("
+        self.properties_create += "PDB_PROPERTY_CREATE("
         # getting property name
         name = list(p.keys())[0]
 
@@ -31,17 +32,17 @@ class PropertyCreateGeneration :
         # getting set function
         set = p[name]['set'] if 'set' in p[name] else 'pdb_property_set_private'
         if set != 'NULL' and set != 'pdb_property_set_private' :
-            self.callbacks_h_create += 'int ' + set + '(pdb_property_e id, u8_t *property_value, size_t size);\n'
+            self.callbacks += 'int ' + set + '(pdb_property_e id, u8_t *property_value, size_t size);\n'
 
         # getting validate function
         validate = p[name]['validate'] if 'validate' in p[name] else 'NULL'
         if validate != 'NULL' and validate != 'pdb_binary' :
-            self.validators_h_create += 'int ' + validate + '(u8_t *property_value, size_t size);\n'
+            self.validators_def += 'int ' + validate + '(u8_t *property_value, size_t size);\n'
 
         # getting get function
         get = p[name]['get'] if 'get' in p[name] else 'pdb_property_get_private'
         if get != 'NULL' and get != 'pdb_property_get_private' :
-            self.callbacks_h_create += 'int ' + get + '(pdb_property_e id, u8_t *property_value, size_t size);\n'
+            self.callbacks += 'int ' + get + '(pdb_property_e id, u8_t *property_value, size_t size);\n'
 
         # getting persistency
         if ('persistent' in p[name]) :
@@ -59,12 +60,14 @@ class PropertyCreateGeneration :
             out = [name, nbytes, validate, get, set, in_flash, observers, name]
 
         # mounting output macro create
-        self.properties_macro_create += ', '.join(out) + ')\n'
+        self.properties_create += ', '.join(out) + ')\n'
 
     def generate_observers(self, obs) :
         for d in obs :
             o = list(d.values())[0]
-            self.observers_h_create += 'PDB_OBSERVER_CREATE(' + o['name'] + ', ' + o['event_queue_size'] + ')\n'
+            self.observers_def += 'PDB_OBSERVER_CREATE(' + o['name'] + ', ' + o['event_queue_size'] + ')\n'
+            if 'diy' in o and o['diy'] == False :
+                self.observers_check += 'PDB_OBSERVER_THREAD_CREATE(' + o['name'] + ', ' + o['thread_size'] + ', ' + o['thread_priority'] + ', ' + o['event_callback'] + ');\n'
 
     def run(self) :
         with open('../properties.yaml', 'r') as file:
@@ -74,14 +77,16 @@ class PropertyCreateGeneration :
             self.generate_observers(observers)
             for p in properties :
                 self.generate_property_create(p)
-            with open('../include/generated/properties.def', 'w') as f :
-                f.write(self.properties_macro_create)
-            with open('../include/generated/validators.def', 'w') as f :
-                f.write(self.validators_h_create)
-            with open('../include/generated/callbacks.def', 'w') as f :
-                f.write(self.callbacks_h_create)
-            with open('../include/generated/observers.def', 'w') as f :
-                f.write(self.observers_h_create)
+            with open('zephyr/include/generated/properties.def', 'w') as f :
+                f.write(self.properties_create)
+            with open('zephyr/include/generated/validators.def', 'w') as f :
+                f.write(self.validators_def)
+            with open('zephyr/include/generated/callbacks.def', 'w') as f :
+                f.write(self.callbacks)
+            with open('zephyr/include/generated/observers.def', 'w') as f :
+                f.write(self.observers_def)
+            with open('zephyr/include/generated/observers_thread.def', 'w') as f :
+                f.write(self.observers_check)
 
 def main():
     pcg = PropertyCreateGeneration()

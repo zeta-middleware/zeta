e74851dfd2387bbeb29d42e00b4fe2f117161ae3
Lucas Peixoto <lucaspeixotoac@gmail.com>
2020-04-29 09:51:18 -0300

    Change basic sample in order to use "zt" prefiz instead "zeta"
    Signed-off-by: Lucas Peixoto <lucaspeixotoac@gmail.com>

--- samples/basic/src/main.c
+++ samples/basic/src/main.c
@@ -10,11 +10,11 @@
 #include "zeta.h"
 #include "zeta_threads.h"
 
-K_SEM_DEFINE(zeta_core_pend_evt, 0, 1);
-K_SEM_DEFINE(zeta_app_pend_evt, 0, 1);
+K_SEM_DEFINE(zt_core_pend_evt, 0, 1);
+K_SEM_DEFINE(zt_app_pend_evt, 0, 1);
 
-zeta_channel_e zeta_core_evt_id;
-zeta_channel_e zeta_app_evt_id;
+zt_channel_e zt_core_evt_id;
+zt_channel_e zt_app_evt_id;
 
 int zeta_validator_different_of_zero(u8_t *data, size_t size)
 {
@@ -25,7 +25,7 @@ void HAL_task(void)
 {
     u8_t data = 1;
     while (1) {
-        zeta_channel_set(ZETA_SENSOR_VAL_CHANNEL, (u8_t *) &data, sizeof(data));
+        zt_channel_set(ZT_SENSOR_VAL_CHANNEL, (u8_t *) &data, sizeof(data));
         k_sleep(K_SECONDS(3));
         data = (data + 1 <= 15) ? data + 1 : 1;
     }
@@ -38,15 +38,15 @@ void CORE_task(void)
 
     while (1) {
         u16_t result = 1;
-        k_sem_take(&zeta_core_pend_evt, K_FOREVER);
-        switch (zeta_core_evt_id) {
-        case ZETA_SENSOR_VAL_CHANNEL:
-            zeta_channel_get(zeta_core_evt_id, (u8_t *) &data, sizeof(data));
+        k_sem_take(&zt_core_pend_evt, K_FOREVER);
+        switch (zt_core_evt_id) {
+        case ZT_SENSOR_VAL_CHANNEL:
+            zt_channel_get(zt_core_evt_id, (u8_t *) &data, sizeof(data));
             printk("[CORE_service_callback] Getting data value %02X\n", data);
             for (u8_t i = 1; i <= data; ++i) {
                 result = result * power;
             }
-            zeta_channel_set(ZETA_POWER_VAL_CHANNEL, (u8_t *) &result, sizeof(result));
+            zt_channel_set(ZT_POWER_VAL_CHANNEL, (u8_t *) &result, sizeof(result));
             break;
         default:
             printk("[CORE_service_callback] receiving an unexpected id channel\n");
@@ -59,14 +59,14 @@ void APP_task(void)
 {
     u8_t fv[4] = {0};
     printk("Hello APP task!\n");
-    zeta_channel_get(ZETA_FIRMWARE_VERSION_CHANNEL, fv, sizeof(fv));
+    zt_channel_get(ZT_FIRMWARE_VERSION_CHANNEL, fv, sizeof(fv));
     printk("Firmware version 0x%02X%02X%02X%02X\n", fv[0], fv[1], fv[2], fv[3]);
     u16_t data = 0;
     while (1) {
-        k_sem_take(&zeta_app_pend_evt, K_FOREVER);
-        switch (zeta_app_evt_id) {
-        case ZETA_POWER_VAL_CHANNEL:
-            zeta_channel_get(zeta_app_evt_id, (u8_t *) &data, sizeof(data));
+        k_sem_take(&zt_app_pend_evt, K_FOREVER);
+        switch (zt_app_evt_id) {
+        case ZT_POWER_VAL_CHANNEL:
+            zt_channel_get(zt_app_evt_id, (u8_t *) &data, sizeof(data));
             printk("[APP_service_callback] Power val is: %04X\n", data);
             break;
         default:
@@ -75,23 +75,23 @@ void APP_task(void)
     }
 }
 
-void CORE_service_callback(zeta_channel_e id)
+void CORE_service_callback(zt_channel_e id)
 {
-    zeta_core_evt_id = id;
-    k_sem_give(&zeta_core_pend_evt);
+    zt_core_evt_id = id;
+    k_sem_give(&zt_core_pend_evt);
 }
 
-void HAL_service_callback(zeta_channel_e id)
+void HAL_service_callback(zt_channel_e id)
 {
 }
 
-void APP_service_callback(zeta_channel_e id)
+void APP_service_callback(zt_channel_e id)
 {
-    zeta_app_evt_id = id;
-    k_sem_give(&zeta_app_pend_evt);
+    zt_app_evt_id = id;
+    k_sem_give(&zt_app_pend_evt);
 }
 
 void main(void)
 {
-    printk("******** ZETA test! *******\n");
+    printk("******** ZETA BASIC SAMPLE! *******\n");
 }

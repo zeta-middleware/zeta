3e7b089ec3a2a57cb7e6b1996e24f790b891828e
Lucas Peixoto <lucaspeixotoac@gmail.com>
2020-05-26 17:24:54 -0300

    Add documentation for ZetaCLIError
    Signed-off-by: Lucas Peixoto <lucaspeixotoac@gmail.com>

--- zeta/zeta_errors.py
+++ zeta/zeta_errors.py
@@ -7,11 +7,25 @@ class ZetaCLIError(Exception):
     """Base class for exceptions in ZetaCLI
     """
     def __init__(self, message, errcode):
+        """ZetaCLIError constructor.
+
+        :param message: Message to be displayed printing error
+        :param errcode: Error code
+        :returns: None
+        :rtype: None
+
+        """
         super().__init__(message)
         self.errcode = errcode
         self.message = message
 
     def handle(self):
+        """Responsible for handling the raise call.
+
+        :returns: None
+        :rtype: None
+
+        """
         print(self)
         exit(self.errcode)
 
